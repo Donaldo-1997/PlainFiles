eb47230e1121788afeb1b031acfa3659f7ee4aa9
donaldojbm7 <donaldojbm7@gmail.com>
2025-06-04 21:38:34 -0500

    CSV with library

---
+++ PlainFiles/CSVWhitLibrary/CSVHelperExample.cs
@@ -0,0 +1,31 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVWhitLibrary;
+
+class CSVHelperExample
+{
+    public void Write(string path, IEnumerable<Person> people)
+    {
+        using var sw = new StreamWriter(path);
+        using var cw = new CsvWriter(sw, CultureInfo.InvariantCulture);
+        cw.WriteRecords(people);
+    } 
+
+    public IEnumerable<Person> Read(string path)
+    {
+        if(!File.Exists(path))
+        {
+            return Enumerable.Empty<Person>();
+        }
+
+        using var sr = new StreamReader(path);
+        using var cr = new CsvReader(sr, CultureInfo.InvariantCulture);
+        return cr.GetRecords<Person>().ToList();
+    }
+}
---
+++ PlainFiles/CSVWhitLibrary/Program.cs
@@ -0,0 +1,18 @@
+﻿using CSVWhitLibrary;
+
+var list = new List<Person> { 
+    new() { Id = 1, Name = "John Doe", Age = "30" },
+    new() { Id = 2, Name = "Jane Smith", Age = "25" },
+    new() { Id = 3, Name = "Sam Brown", Age = "40" },
+    new() { Id = 4, Name = "Alice Johnson", Age = "28" }
+};
+
+var helper = new CSVHelperExample();
+helper.Write("D:\\ITM\\3 SEMESTRE\\Estructura de Datos y Laboratorio\\people.csv", list);
+
+var readList = helper.Read("D:\\ITM\\3 SEMESTRE\\Estructura de Datos y Laboratorio\\people.csv");
+
+foreach (var person in readList)
+{
+    Console.WriteLine($"Id: {person.Id}, Name: {person.Name}, Age: {person.Age}");
+} 
